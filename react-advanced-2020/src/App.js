cb797206494e53c75ab1bd4e171adad5ea1c6b48
Leobardo Pineda <leobapineda@gmail.com>
2022-07-21 10:35:08 -0500

    Feat: add short-circuit evaluation and ternary operator in tutorial 3 setup 2

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import UseEffectBasics from "./tutorial/2-useEffect/final/1-useEffect-basics";
-import UseEffectBasics from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import UseEffectBasics from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 
 function App() {
   return (
--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
-
-  return <h2>short circuit</h2>;
+  const [click, setClick] = useState(false);
+  return (
+    <>
+      <button onClick={() => setClick((prevState) => !prevState)}>
+        click me
+      </button>
+      <h1>{click ? "click is true" : "click is false"}</h1>
+      {click && <h1>button is clicked</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
