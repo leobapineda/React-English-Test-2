08fd304f08847f8474259fe94386d9514f1d5377
Leobardo Pineda <leobapineda@gmail.com>
2022-07-20 21:04:48 -0500

    Feat: add conditional renderign to fetch data in tutorial 3 setup 1

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import UseEffectBasics from "./tutorial/2-useEffect/final/1-useEffect-basics";
-import UseEffectBasics from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import UseEffectBasics from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 
 function App() {
   return (
--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,51 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [loading, setLoagind] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState({});
+
+  async function fetchDate() {
+    try {
+      const response = await fetch(url);
+      console.log(response);
+
+      if (response.status >= 200 && response.status <= 299) {
+        const userInfo = await response.json();
+        console.log(userInfo);
+        setUser(userInfo);
+        setLoagind(false);
+        return userInfo;
+      } else {
+        setIsError(true);
+        setLoagind(false);
+      }
+    } catch (err) {
+      setLoagind(false);
+      setIsError(true);
+    }
+  }
+
+  useEffect(() => {
+    fetchDate();
+  }, []);
+
+  if (loading) {
+    return <h3>loading...</h3>;
+  }
+
+  if (isError) {
+    return <h3>Error...</h3>;
+  }
+
+  if (user) {
+    return (
+      <>
+        <h3>{user.login}</h3>
+        <img src={user.avatar_url} alt={user.login} />
+      </>
+    );
+  }
 };
 
 export default MultipleReturns;
