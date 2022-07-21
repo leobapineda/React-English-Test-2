a4d3c4ac4d73a63bb3f7fb988753a2a269db86e3
Leobardo Pineda <leobapineda@gmail.com>
2022-07-21 10:06:25 -0500

    Feat: add localStorage to conditional renderign to fetch data in tutorial 3 setup 1

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -4,25 +4,39 @@ const MultipleReturns = () => {
   const [loading, setLoagind] = useState(true);
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState({});
-
+  const obj = {
+    login: "leo",
+    avatar_url:
+      "https://i0.wp.com/uncomocorreo.com/wp-content/uploads/2018/03/lol-meme.jpg?resize=415%2C501&ssl=1",
+    bio: "nadaquever@.com",
+    company: "no trabajo me mantienen",
+  };
   async function fetchDate() {
     try {
       const response = await fetch(url);
       console.log(response);
-
       if (response.status >= 200 && response.status <= 299) {
         const userInfo = await response.json();
-        console.log(userInfo);
+        const local = localStorage.getItem("user");
+        if (local) {
+          setUser(JSON.parse(local));
+          console.log(JSON.parse(local));
+          setLoagind(false);
+          return;
+        }
         setUser(userInfo);
         setLoagind(false);
+        localStorage.setItem("user", JSON.stringify(obj));
         return userInfo;
       } else {
         setIsError(true);
         setLoagind(false);
+        return;
       }
     } catch (err) {
       setLoagind(false);
       setIsError(true);
+      return;
     }
   }
 
@@ -43,6 +57,8 @@ const MultipleReturns = () => {
       <>
         <h3>{user.login}</h3>
         <img src={user.avatar_url} alt={user.login} />
+        <div>{user.bio}</div>
+        <div>{user.company}</div>
       </>
     );
   }
