dbc80020375916d3b2d735301d5aa891e43c1998
Leobardo Pineda <leobapineda@gmail.com>
2022-07-25 12:17:48 -0500

    Test: add form using useState

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 // import UseEffectBasics from "./tutorial/5-useRef/setup/1-useRef-basics";
-import UseEffectBasics from "./tutorial/6-useReducer/setup/index";
+import Setup from "./tutorial/6-useReducer/setup/index";
 // import UseEffectBasics from "./tutorial/6-useReducer/final/index";
 
 function App() {
   return (
     <>
-      <UseEffectBasics />
+      <Setup />
     </>
   );
 }
