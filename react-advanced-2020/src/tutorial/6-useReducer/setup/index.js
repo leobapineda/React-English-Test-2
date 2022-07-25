f244982394b0f1868416f2ba7853d284f3ca2090
Leobardo Pineda <leobapineda@gmail.com>
2022-07-25 13:51:04 -0500

    Test: add form using useReducer
    The initial state of useReducer is an object with data with the users data and empty as a boolean false

--- react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
@@ -5,38 +5,81 @@ import data  from '../../../data';
 
 
 const Index = () => {
-const [name, setName] = useState("")
-const [isModal, setIsModal] = useState(false)
-const [people, setPeople] = useState(data)
 
+  const ACTIONS = {
+    ADD: "add",
+    REMOVE: "remove",
+    EMPTY: "empty",
+  };
 
+  function reducer(users, action) {
+    switch (action.type) {
+      case ACTIONS.ADD:
+        console.log("add");
+        return { data: [...users.data, action.payload], empty: false };
+      case ACTIONS.REMOVE:
+        console.log("remove");
+        return { data: action.payload, empty: false };
+      case ACTIONS.EMPTY:
+        console.log("default");
+        return { data: users.data, empty: action.payload };
+    }
+  }
+
+  const [users, dispatch] = useReducer(reducer, {data, empty:false});
+  const [person, setPerson] = useState("")
+  // me esta metiendo a mi data un array con los otros tres elementos
 function handleSubmit(e) {
-  console.log("submit");
   e.preventDefault()
-  if(name) {
-  console.log("if");
+  addUser();
+setPerson("")
+}
 
-   setPeople(prevPeopls => {
-    return [...prevPeopls, {name:name, id:Date.now()}];
-   })
+function addUser() {
+  if(person) {
+    dispatch({ type: ACTIONS.ADD, payload: {name:person, id: Date.now()} });
   } else {
-    console.log("else");
-    setIsModal(prev => !prev)
+    dispatch({ type: ACTIONS.EMPTY, payload: true });
   }
 }
 
+function removeUser(id){
+  const newUsers = users.data.filter((user) => user.id !== id)
+  console.log(newUsers);
+      dispatch({ type: ACTIONS.REMOVE, payload: newUsers });
+}
+console.log("user");
+console.log(users);
+console.log("user data");
+console.log(users.data);
   return (
     <>
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input onChange={(e) => setName(e.target.value)} type="text" name="" id="" />
+        <input
+          value={person}
+          onChange={(e) => setPerson(e.target.value)}
+          type="text"
+          name=""
+          id=""
+        />
         <button>submit</button>
       </form>
-      {isModal && <Modal />}
       <div>
-
-        {people.map((person) => {
-          const {id, name} = person
-          return <h4 key={id} >{name}</h4>
+        {users.empty && <Modal />}
+        {users.data.map((person) => {
+          const { id, name } = person;
+          return (
+            <div style={{ margin: "1rem" }} key={id}>
+              <span key={id}>{name}</span>
+              <button
+                onClick={() => {
+                  removeUser(id);
+                }}
+              >
+                remove
+              </button>
+            </div>
+          );
         })}
       </div>
     </>
