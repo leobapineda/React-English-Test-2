57cca8bc63dab1ade43440d8fac7d1650e3f7968
Leobardo Pineda <leobapineda@gmail.com>
2022-07-27 21:48:26 -0500

    Feat: add project 8 lorem ipsum using useRef instead of useState to save input value

---
+++ react-projects/08-lorem-ipsum/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useRef } from "react";
+import data from "./data";
+function App() {
+  const [lorem, setLorem] = useState([]);
+  const length = useRef(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const paragrapgNumber = Number(length.current.value);
+    if (paragrapgNumber <= 0) {
+      setLorem(data.slice(0, 1));
+      return;
+    }
+    setLorem(data.slice(0, paragrapgNumber));
+  }
+
+  console.log("render");
+  return (
+    <section className="section-center">
+      <form className="lorem-form" onSubmit={(e) => handleSubmit(e)}>
+        <label > Paragraphs
+          <input defaultValue={0} ref={length} type="number" />
+        </label>
+        <button className="btn">Generate</button>
+      </form>
+      <div>
+        {lorem.map((item, index) => {
+          return (
+            <div key={index}>
+              <h5>{index + 1}</h5>
+              <p>{item}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default App;
