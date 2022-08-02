b7530266d4c7010583b5b86eee678fde7e2bceeb
Leobardo Pineda <leobapineda@gmail.com>
2022-08-02 16:16:07 -0500

    Feat: add project 11 css and html

---
+++ react-projects/11-navbar/src/Navbar.js
@@ -0,0 +1,49 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
+
+const Navbar = () => {
+
+
+  // change 
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container">
+          <ul className="links">
+            {links.map((link) => {
+              const { id, text, url } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((red) => {
+            const { id, icon, url } = red;
+            return (
+              <li key={id}>
+                <a target="_blank" href={url}>
+                  {icon}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
