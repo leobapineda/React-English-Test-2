8ca1178305a1a5bbbd61bc884508c310c4ffcab7
Leobardo Pineda <leobapineda@gmail.com>
2022-08-03 22:17:15 -0500

    Test: add interview problem

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -11,23 +11,96 @@
 // }
 // export default App;
 
-import React, { useContext } from "react";
-import MoreInfo from "./components/MoreInfo";
-// custom hook
-import userContext from "./context/UserContext";
+import React, { useEffect, useState } from "react";
+
+const itemApi = () =>
+  new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve([
+        { gruopId: 1, nombre: "Mario" },
+        { gruopId: 2, nombre: "Lucia" },
+        { gruopId: 1, nombre: "Luis" },
+        { gruopId: 1, nombre: "Carmen" },
+        { gruopId: 3, nombre: "Boris" },
+        { gruopId: 2, nombre: "Oscar" },
+      ]);
+    }, 500);
+  });
 
 function App() {
-  const userDate = {
-    name: "leo",
-    age: 24,
-  };
+  const [data, setData] = useState([]);
+  // obtener al renderizar
+  // una vez obtenida modificar
 
-  console.log(userContext);
-  return (
-    <userContext.Provider value={userDate}>
-      <h1>useContext</h1>
-      <MoreInfo />
-    </userContext.Provider>
-  );
+  useEffect(() => {
+    const apiRes = itemApi()
+      .then((res) => setData(res))
+      .catch((err) => console.log(err));
+    console.log(data);
+    // const newArray = data.map((user) => {
+    //   let group1 = [];
+    //   let group2 = [];
+    //   let group3 = [];
+    //   if (user.gruopId === 1) {
+    //     group1.push(user);
+    //   } else if (user.gruopId === 2) {
+    //     group2.push(user);
+    //   } else if (user.gruopId === 3) {
+    //     group3.push(user);
+    //   }
+    //   return [1, 2, 3];
+    // });
+    // console.log(newArray);
+
+    // no me regresa un array, porque
+  }, []);
+
+      let group1 = [];
+      let group2 = [];
+      let group3 = [];
+
+  data.map((user) => {
+    if (user.gruopId === 1) {
+      group1.push(user);
+    } else if (user.gruopId === 2) {
+      group2.push(user);
+    } else if (user.gruopId === 3) {
+      group3.push(user);
+    }
+    return 
+  });
+
+  let newArray = [group1, group2, group3];
+  console.log(newArray);
+  // ya tengo el array
+    // por cada array me tiene que dar un nuevo nuevo h1
+  
+  console.log(data);
+
+  return <div>{newArray.map((arrayUsers, index) => {
+    return (
+      <div key={index}>
+        <h4>group {index + 1}</h4>
+        <ul>
+          {arrayUsers.map((user) => {
+            const { gruopId, nombre } = user;
+            return <li key={nombre}>{nombre}</li>;
+          })}
+        </ul>
+      </div>
+    );
+    // por cada array regresar un 
+    // h1
+    // ul
+
+
+  })}</div>;
 }
+
+// group 1:
+// ul
+// li name
+// li name
+
+
 export default App;
