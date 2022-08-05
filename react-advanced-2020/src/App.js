e0344f0ff891e903e0a5c6167c6b0466a31e9363
Leobardo Pineda <leobapineda@gmail.com>
2022-08-05 10:54:53 -0500

    Feat: finished customHook tutorial 9 setup

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,50 +1,11 @@
-// import React from "react";
-// import Setup from "./tutorial/9-custom-hooks/final/1-fetch-example";
-// // import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
-
-// function App() {
-//   return (
-//     <>
-//       <Setup />
-//     </>
-//   );
-// }
-// export default App;
-
-// import React from "react";
-// import { useEffect } from "react";
-// import { useState } from "react";
-// import useLocalStorage from "./hooks/useLocalStorage";
-// function App() {
-//   const [local, setLocal] = useLocalStorage("valor", "")
-
-//   return (
-//     <>
-//       <input
-//         onChange={(e) => setLocal(e.target.value)}
-//         type="text"
-//         name=""
-//         id=""
-//         value={local}
-//       />
-//     </>
-//   );
-// }
-// export default App;
-
 import React from "react";
-import { useState } from "react";
-import useUpdateLogger from "./hooks/useUpdateLogger";
+// import Setup from "./tutorial/9-custom-hooks/final/1-fetch-example";
+import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
+
 function App() {
-  const [text, setText] = useState("");
-
-  useUpdateLogger(text);
-
-  console.log("App");
-
   return (
     <>
-      <input onChange={(e) => setText(e.target.value)} type="text" />
+      <Setup />
     </>
   );
 }
--- react-advanced-2020/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ react-advanced-2020/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+  console.log(2);
+  return { loading, products };
+};
