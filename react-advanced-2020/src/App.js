e33b304794f644199498144f09705d7081a08238
Leobardo Pineda <leobapineda@gmail.com>
2022-08-08 16:32:18 -0500

    Feat: add basic useMemo and useCallBack

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,10 +1,42 @@
-import React from "react";
-// import Setup from "./tutorial/9-custom-hooks/final/1-fetch-example";
-import Setup from "./tutorial/11-react-router/setup";
+// import React from "react";
+// // import Setup from "./tutorial/9-custom-hooks/final/1-fetch-example";
+// import Setup from "./tutorial/11-react-router/setup";
+
+// function App() {
+//   return (
+//       <Setup />
+//   );
+// }
+// export default App;
+import React, { useState, useCallback } from "react";
+import List from "./List";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [isDark, setIsDark] = useState(false);
+
+  const getItems = useCallback(() => {
+    return [number, number + 1, number + 2];
+  }, [number]);
+
+  const styleTheme = {
+    backgroundColor: isDark ? "black" : "white",
+    color: isDark ? "white" : "black",
+  };
+
   return (
-      <Setup />
+    <>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setIsDark((prev) => !prev)}>toggle theme</button>
+      <div style={styleTheme}>
+        <List getItems={getItems} />
+      </div>
+    </>
   );
 }
+
 export default App;
